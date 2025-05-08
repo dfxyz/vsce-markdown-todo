223c02238cfa051127dd01b8364ecac280f08fa5
DF_XYZ <dfxyz1@gmail.com>
2025-05-09 00:13:49 +0800

    feat: add text decoration for TODO state keywords

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,18 @@ import * as vscode from "vscode";
 
 const TODO_KEYWORDS = ["TODO", "DONE"];
 const LINE_REGEX = /^((#{1,6}|\s*[-+*])\s+)(.*)$/;
+const DECORATE_LINE_REGEX = /^((#{1,6}|\s*[-+*])\s+)(TODO|DONE)\s+.*$/;
+
+const TODO_DECORATION = vscode.window.createTextEditorDecorationType({
+  color: "#C05430",
+  fontWeight: "bold",
+});
+const DONE_DECORATION = vscode.window.createTextEditorDecorationType({
+  color: "#008020",
+  fontWeight: "bold",
+});
+
+let DECORATED_EDITORS = new WeakSet<vscode.TextEditor>();
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
@@ -15,6 +27,18 @@ export function activate(context: vscode.ExtensionContext) {
       "markdown-todo.cycleTodoStateBackward",
       () => cycleTodoState(false),
     ),
+  );
+
+  initDecoration();
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeTextDocument((event) => {
+      updateDecorationOnDocumentTextChange(event);
+    }),
+  );
+  context.subscriptions.push(
+    vscode.window.onDidChangeVisibleTextEditors((editors) => {
+      updateDecorationOnVisibleTextEditorChange(editors);
+    }),
   );
 }
 
@@ -69,4 +93,81 @@ function cycleTodoState(cycleForward = true) {
     );
     editBuilder.replace(range, newLine);
   });
+}
+
+function initDecoration() {
+  for (const editor of vscode.window.visibleTextEditors) {
+    updateDecoration(editor);
+  }
+}
+
+function updateDecorationOnDocumentTextChange(
+  event: vscode.TextDocumentChangeEvent,
+) {
+  for (const editor of vscode.window.visibleTextEditors) {
+    if (editor.document === event.document) {
+      updateDecoration(editor);
+    }
+  }
+}
+
+function updateDecorationOnVisibleTextEditorChange(
+  editors: readonly vscode.TextEditor[],
+) {
+  const newDecoratedSet = new WeakSet();
+  const notDecoratedEditors = [];
+
+  for (const editor of editors) {
+    if (DECORATED_EDITORS.has(editor)) {
+      newDecoratedSet.add(editor);
+    } else {
+      notDecoratedEditors.push(editor);
+    }
+  }
+  DECORATED_EDITORS = newDecoratedSet;
+
+  for (const editor of notDecoratedEditors) {
+    updateDecoration(editor);
+  }
+}
+
+function updateDecoration(editor: vscode.TextEditor) {
+  if (editor.document.languageId !== "markdown") {
+    return;
+  }
+  const todoDecorations = [];
+  const doneDecorations = [];
+
+  const lineCount = editor.document.lineCount;
+  for (let lineNumber = 0; lineNumber < lineCount; lineNumber++) {
+    const line = editor.document.lineAt(lineNumber).text;
+
+    let match = line.match(DECORATE_LINE_REGEX);
+    if (match === null) {
+      continue;
+    }
+
+    const startCount = match[1].length;
+    const keyword = match[3];
+    if (keyword === "TODO") {
+      todoDecorations.push(
+        new vscode.Range(
+          new vscode.Position(lineNumber, startCount),
+          new vscode.Position(lineNumber, startCount + keyword.length),
+        ),
+      );
+    } else {
+      doneDecorations.push(
+        new vscode.Range(
+          new vscode.Position(lineNumber, startCount),
+          new vscode.Position(lineNumber, startCount + keyword.length),
+        ),
+      );
+    }
+  }
+
+  editor.setDecorations(TODO_DECORATION, todoDecorations);
+  editor.setDecorations(DONE_DECORATION, doneDecorations);
+
+  DECORATED_EDITORS.add(editor);
 }
