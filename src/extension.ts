5c7b7a2293ca7e15aa31af0a4af5f086697ff827
DF_XYZ <dfxyz1@gmail.com>
2025-05-08 23:08:14 +0800

    feat: support cycling TODO state backward

--- src/extension.ts
+++ src/extension.ts
@@ -1,59 +1,65 @@
 import * as vscode from "vscode";
 
-const KEYWORDS = ["TODO", "WIP", "DONE"];
-const HEADING_LINE_REGEX = /^(#{1,6})\s+(.*)$/;
-const UNSORTED_LIST_REGEX = /^(\s*[-+*])\s+(.*)$/;
+const TODO_KEYWORDS = ["TODO", "DONE"];
+const LINE_REGEX = /^((#{1,6}|\s*[-+*])\s+)(.*)$/;
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand(
-    "markdown-todo.cycleTodoState",
-    cycleTodoState,
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "markdown-todo.cycleTodoState",
+      cycleTodoState,
+    ),
   );
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "markdown-todo.cycleTodoStateBackward",
+      () => cycleTodoState(false),
+    ),
+  );
 }
 
 export function deactivate() {}
 
-function cycleTodoState() {
+function cycleTodoState(cycleForward = true) {
   const editor = vscode.window.activeTextEditor;
   if (!editor) {
+    vscode.window.showErrorMessage("No active text editor.");
     return;
   }
 
   const document = editor.document;
   if (document.languageId !== "markdown") {
+    vscode.window.showErrorMessage("Only supported in markdown files.");
     return;
   }
 
   const line = editor.document.lineAt(editor.selection.active.line);
   const text = line.text;
 
-  let match = text.match(HEADING_LINE_REGEX);
-  if (!match) {
-    match = text.match(UNSORTED_LIST_REGEX);
-  }
-  if (!match) {
+  const match = text.match(LINE_REGEX);
+  if (match === null) {
+    vscode.window.showErrorMessage("Not supported in current context.");
     return;
   }
-  const prefix = match[1];
-  const mainContent = match[2];
+  const punctPart = match[2] + " ";
+  const textPart = match[3];
 
-  let content = mainContent;
-  let keywordIndex = -1;
-  for (const [i, v] of Object.entries(KEYWORDS)) {
-    if (mainContent.startsWith(`${v} `)) {
-      keywordIndex = Number(i);
-      content = mainContent.slice(v.length + 1);
+  let remainingTextPart = textPart.trim();
+  let currentKeywordIndex = cycleForward ? -1 : TODO_KEYWORDS.length;
+  for (const [i, v] of Object.entries(TODO_KEYWORDS)) {
+    if (textPart.startsWith(v + " ")) {
+      currentKeywordIndex = Number(i);
+      remainingTextPart = textPart.slice(v.length).trim();
       break;
     }
   }
-  keywordIndex += 1;
-  if (keywordIndex >= KEYWORDS.length) {
-    keywordIndex = -1;
+  const nextKeywordIndex = currentKeywordIndex += cycleForward ? 1 : -1;
+  let keywordPart = TODO_KEYWORDS[nextKeywordIndex] ?? "";
+  if (keywordPart !== "") {
+    keywordPart += " ";
   }
-  const newKeyword = keywordIndex >= 0 ? `${KEYWORDS[keywordIndex]} ` : '';
-  const newLine = `${prefix} ${newKeyword}${content}`;
 
+  const newLine = `${punctPart}${keywordPart}${remainingTextPart}`;
   editor.edit((editBuilder) => {
     const range = new vscode.Range(
       line.lineNumber,
